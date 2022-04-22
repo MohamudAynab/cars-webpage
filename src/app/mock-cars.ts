f2ea57b0119a8988266f9b06691c446bed839a0e
Duwe Aynab <mfaynab88@gmail.com>
2022-04-22 10:07:01 -0400

    initial commit

---
+++ src/app/mock-cars.ts
@@ -0,0 +1,11 @@
+import { Car } from './car';
+
+export const CARS: Car[] =  [
+    {make:'Ford', model:'F-150', year:2020},
+    {make:'Toyota', model:'Corrolla', year:2021},
+    {make:'Honda', model:'Accord', year:2022},
+    {make:'Jeep', model:'Cherokee', year:2019},
+    {make:'Kia', model:'Sorrento', year:2018,},
+    {make:'Tesla', model:'C', year:2017},
+    {make:'Cadillac', model:'Escalade', year:2015},
+];
